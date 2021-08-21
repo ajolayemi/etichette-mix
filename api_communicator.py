9be02aa9a8c019351b698b077addf654ae9e2e84
jolayemi_local <ajolayemi82@gmail.com>
2021-08-21 13:08:24 +0200

    Wrote WorkerThread class moving api_communicator.py inside the class

--- api_communicator.py
+++ api_communicator.py
@@ -5,6 +5,8 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
+from PyQt5.QtCore import QObject, pyqtSignal
+
 # helper_modules is a self defined module
 from helper_modules import helper_functions
 
@@ -26,63 +28,76 @@ WRITING_RANGE_NAME = json_file_content.get('wb_range_to_write')
 FILE_RANGE_TO_CLEAR = json_file_content.get('wb_range_to_clear')
 
 
-def api_communicator():
-    # Create the service that will be used
-    service = build('sheets', 'v4', credentials=creds)
+class WorkerThread(QObject):
+    # Custom signals
+    progress = pyqtSignal(int)
+    finished = pyqtSignal()
+    unfinished = pyqtSignal()
 
-    # Call the Google Sheets API
-    sheet = service.spreadsheets()
+    def __init__(self):
+        super(WorkerThread, self).__init__()
 
-    # Clear existing data in spreadsheet cells where new data will be written
-    # To learn more about this, check Google Sheets API reference docs
-    clear_data_request = service.spreadsheets().values().batchClear(spreadsheetId=SPREADSHEET_ID,
-                                                                    body={'ranges': FILE_RANGE_TO_CLEAR})
-    clear_data_request.execute()
+    def api_communicator(self):
+        # Create the service that will be used
+        service = build('sheets', 'v4', credentials=creds)
 
-    sheet_values = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                      range=READING_RANGE).execute()
+        # Call the Google Sheets API
+        sheet = service.spreadsheets()
 
-    # The returned sheet value will be a nested list
-    # The first value in a list is the label's content
-    # The second value is the current product ID
-    # The third value is the current product producer
-    # The fourth value is the current product ordered quantity
+        # Clear existing data in spreadsheet cells where new data will be written
+        # To learn more about this, check Google Sheets API reference docs
+        clear_data_request = service.spreadsheets().values().batchClear(spreadsheetId=SPREADSHEET_ID,
+                                                                        body={'ranges': FILE_RANGE_TO_CLEAR})
+        clear_data_request.execute()
 
-    all_values = sheet_values.get('values', [])[1:]
+        sheet_values = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                          range=READING_RANGE).execute()
 
-    sorted_values = sorted(all_values, key=lambda x: (x[2], x[1]), reverse=True)
+        # The returned sheet value will be a nested list
+        # The first value in a list is the label's content
+        # The second value is the current product ID
+        # The third value is the current product producer
+        # The fourth value is the current product ordered quantity
 
-    sorted_values_len = len(sorted_values)
+        all_values = sheet_values.get('values', [])[1:]
 
-    current_row = 2
-    final_data = []
-    for current_index in range(sorted_values_len):
-        if current_index == 0:
-            final_data.append([sorted_values[0][2]])
-            final_data.append(sorted_values[current_index])
+        sorted_values = sorted(all_values, key=lambda x: (x[2], x[1]), reverse=True)
 
-        elif current_index == sorted_values_len - 1:
-            final_data.append(sorted_values[current_index])
-            final_data.append([sorted_values[current_index][2]])
+        sorted_values_len = len(sorted_values)
 
-        elif sorted_values[current_index][2] != sorted_values[current_index + 1][2]:
-            # First append the current label content
-            final_data.append(sorted_values[current_index])
-            # Then append the current producer
-            final_data.append([sorted_values[current_index][2]])
-            # Then append the next producer
-            final_data.append([sorted_values[current_index + 1][2]])
+        current_row = 2
+        final_data = []
+        for current_index in range(sorted_values_len):
+            self.progress.emit(helper_functions.get_percentage_of(
+                current_index, sorted_values_len
+            ))
 
-        else:
-            final_data.append(sorted_values[current_index])
+            if current_index == 0:
+                final_data.append([sorted_values[0][2]])
+                final_data.append(sorted_values[current_index])
 
-    write_data_request = service.spreadsheets().values().append(spreadsheetId=SPREADSHEET_ID,
-                                                                range=f'{WRITING_RANGE_NAME}{current_row}',
-                                                                valueInputOption='USER_ENTERED',
-                                                                insertDataOption='OVERWRITE',
-                                                                body={'values': final_data})
-    response = write_data_request.execute()
-    return response
+            elif current_index == sorted_values_len - 1:
+                final_data.append(sorted_values[current_index])
+                final_data.append([sorted_values[current_index][2]])
+
+            elif sorted_values[current_index][2] != sorted_values[current_index + 1][2]:
+                # First append the current label content
+                final_data.append(sorted_values[current_index])
+                # Then append the current producer
+                final_data.append([sorted_values[current_index][2]])
+                # Then append the next producer
+                final_data.append([sorted_values[current_index + 1][2]])
+
+            else:
+                final_data.append(sorted_values[current_index])
+
+        write_data_request = service.spreadsheets().values().append(spreadsheetId=SPREADSHEET_ID,
+                                                                    range=f'{WRITING_RANGE_NAME}{current_row}',
+                                                                    valueInputOption='USER_ENTERED',
+                                                                    insertDataOption='OVERWRITE',
+                                                                    body={'values': final_data})
+        response = write_data_request.execute()
+        return response
 
 
 if __name__ == '__main__':
